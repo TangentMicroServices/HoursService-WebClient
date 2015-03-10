92bb73036dacefb9f02ed19a6ea297b9370f24c8
Ian Roberts <ian@tangentsolutions.co.za>
2015-03-10 15:22:01 +0200

    Added basic scafolding application

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # HoursService-WebClient
+
 Calling the hours service with authentication. Using angular
 
+## Setting Up
+
+1. Build the application
+
+		grunt
+		
+2. Run the application
+
+		grunt serve
---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hoursApp
+ * @description
+ * # hoursApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hoursApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
